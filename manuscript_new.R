e7b002ade7a242e37c527fbbaf8aca43146d7ef6
ashleyzhou972 <ashley.n.zhou@gmail.com>
2018-05-09 15:41:30 -0500

    cleaned manuscript

--- manuscript_new.R
+++ manuscript_new.R
@@ -1,9 +1,9 @@
 setwd( '~/Documents/labwork/crowdsource/')
 library(RColorBrewer)
 library(plot3D)
-source('./clean_filter.R')
+source('./git/clean_filter.r')
 #clean_filter('20160928/',1)
-clean_filter('20161215/',2)
+#clean_filter('20161215/',2)
 
 per_user_image<-read.csv('./20161215/per_user_image_filter.csv')
 perbox<-read.csv('./20161215/perbox.csv')
@@ -12,14 +12,6 @@ perbox = perbox[,-1]
 #par(mfrow=c(1,3))
 
 #Collapse perbox data to per user image and count perfect recall
-
-
-lower = quantile(per_user_image$question.elapsed,probs=c(0.05,0.95))[1]
-upper = quantile(per_user_image$question.elapsed,probs=c(0.05,0.95))[2]
-per_user_image_nooutlier = per_user_image[per_user_image$question.elapsed>=lower & per_user_image$question.elapsed<=upper,]
-
-#nooutlier = read.csv('./outlierremoved.csv')
-timepred = read.csv('./timepred.csv')
 
 ###Generating density plots
 plotDensity<-function(perbox,group){
@@ -63,104 +55,6 @@ title('Non-Master MTurk')
 vioplot(qe_sona_easy,qe_sona_hard,names=c('easy','hard'),col=cols[3])
 title('Course Credit')
 
-####Generating the time vs time plot
-#estimates for the linear regression are from SAS
-#only one outlier (15000 seconds removed from SONA group)
-plot(per_user_image$question.ordinal,per_user_image$question.elapsed,col="#00000033",
-     ylim = c(0,500),pch=20,xlab="Question Ordinal Index", ylab="Time Spent per Question (in seconds)")
-#plot(timepred$question_ordinal,timepred$Pred,col="red",pch=16)
-cols = brewer.pal(3,"Dark2")
-#Master
-abline(81.6951,-0.7421,col=cols[1],lwd=3)
-#Turker
-abline(68.6631,-0.5728,col=cols[2],lwd=3)
-#SONA
-abline(45.8278,-0.4236,col=cols[3],lwd=3)
-legend('topright',col=brewer.pal(3,'Dark2'),legend = c("Master MTurk","non-Master MTurk","Course Credit"),lwd= c(3,3,3))
-
-#5% and 95% outliers removed
-plot(per_user_image_nooutlier$question.ordinal,per_user_image_nooutlier$question.elapsed,
-     ylim=c(0,120),col="#00000033",pch=20,xlab="Question Ordinal Index", ylab="Time Spent per Question (in seconds)")
-cols = brewer.pal(3,"Dark2")
-#Master
-abline(49.0918,-0.3042,col=cols[1],lwd=3)
-#Turker
-abline(48.3152,-0.3250,col=cols[2],lwd=3)
-#SONA
-abline(38.1910,-0.3126,col=cols[3],lwd=3)
-legend('topright',col=brewer.pal(3,'Dark2'),legend = c("Master MTurk","non-Master MTurk","Course Credit"),lwd= c(3,3,3))
-
-
-#Generate accuracy over time plot
-#does accuracy change as time goes by
-plot(per_user_image$question.ordinal,per_user_image$fmean,ylim=c(0,1),col="#00000033",pch=20,xlab = 'Question Ordinal Index',
-     ylab = 'Mean F-value')
-cols = brewer.pal(3,"Dark2")
-#Master pvalue=0.0457
-abline(0.7909,-0.00008,col=cols[1],lwd=3)
-#Turker pvalue<0.0001
-abline(0.8100,-0.00027,col=cols[2],lwd=3)
-#SONA pvalue<0.0001
-abline(0.7256,-0.00095,col=cols[3],lwd=3)
-legend('bottomleft',col=brewer.pal(3,'Dark2'),legend = c("Master MTurk","non-Master MTurk","Course Credit"),lwd= c(3,3,3))
-
-
-#Generate accuracy vs time plot
-#is accuracy correlated with time spent on that question
-lmfit=lm(per_user_image_nooutlier$fmean~per_user_image_nooutlier$question.elapsed)
-summary(lmfit)
-plot(per_user_image_nooutlier$question.elapsed,per_user_image_nooutlier$fmean,
-     xlab = 'Time Spent per Question (in seconds)',ylab='mean F value',
-     col="#00000033",pch=20)
-#master pvalue<0.0001
-abline(0.7869,0.000220,col=cols[1],lwd=3)
-#turker pvalue <0.0001
-abline(0.7917,0.000226,col=cols[2],lwd=3)
-#sona pvalue<0.0001
-abline(0.6633,0.000106,col=cols[3],lwd=3)
-legend('bottomright',col=brewer.pal(3,'Dark2'),legend = c("Master MTurk","non-Master MTurk","Course Credit"),lwd= c(3,3,3))
-
-
-#Time over difficult and easy images
-summary(lm(question.elapsed~as.factor(difficulty),data=per_user_image_nooutlier))
-#significant without random effect, once add in random effect, no longer significant
-master_nooutlier = per_user_image_nooutlier[per_user_image_nooutlier$type==1,]
-turker_nooutlier = per_user_image_nooutlier[per_user_image_nooutlier$type==2,]
-sona_nooutlier = per_user_image_nooutlier[per_user_image_nooutlier$typhie==3,]
-#The following is on fmean
-height_master = c(mean(master_nooutlier[master_nooutlier$difficulty==1,'fmean']),mean(master_nooutlier[master_nooutlier$difficulty==2,'fmean']))
-height_turker = c(mean(turker_nooutlier[turker_nooutlier$difficulty==1,'fmean']),mean(turker_nooutlier[turker_nooutlier$difficulty==2,'fmean']))
-height_sona = c(mean(sona_nooutlier[sona_nooutlier$difficulty==1,'fmean']),mean(sona_nooutlier[sona_nooutlier$difficulty==2,'fmean']))
-heights = cbind(height_master,height_turker,height_sona)
-barplot(heights,beside=T,names=c('Master MTurk','Non-Master MTurk','Course Credit'),ylim=c(0,1),col=cols[1:2],xlab='Difficulty',ylab='Mean F value')
-legend('topright',c('easy','hard'),col=cols[1:2],pch=c(15,15))
-#The following is on time
-height_master = c(mean(master_nooutlier[master_nooutlier$difficulty==1,'question.elapsed']),mean(master_nooutlier[master_nooutlier$difficulty==2,'question.elapsed']))
-height_turker = c(mean(turker_nooutlier[turker_nooutlier$difficulty==1,'question.elapsed']),mean(turker_nooutlier[turker_nooutlier$difficulty==2,'question.elapsed']))
-height_sona = c(mean(sona_nooutlier[sona_nooutlier$difficulty==1,'question.elapsed']),mean(sona_nooutlier[sona_nooutlier$difficulty==2,'question.elapsed']))
-heights = cbind(height_master,height_turker,height_sona)
-barplot(heights,beside=T,names=c('Master MTurk','Non-Master MTurk','Course Credit'),col=cols[1:2],xlab='Difficulty',ylab='Mean Question Time (in seconds) ')
-legend('topright',c('easy','hard'),col=cols[1:2],pch=c(15,15))
-
-
-#Binomial (# of perfect recalls) GLM
-library(lme4)
-glmfit=glm(cbind(recallCount,tasselCount-recallCount)~as.factor(type),data=per_user_image,family=binomial(logit))
-glmmfit=glmer(cbind(recallCount,tasselCount-recallCount)~as.factor(type)+(1|user)+(1|image),data=per_user_image,family=binomial(logit))
-summary(glmmfit)
-anova(glmmfit)
-#lme4
-
-
-
-
-#Questions:
-#ANOVA normality
-#Outlier removed or not (time)
-#
-
-####################################################
-#####updated 20171012###############################
 ##Use log-time for question.elapsed
 logtime = log(per_user_image$question.elapsed)
 per_user_image = cbind(per_user_image,logtime)
@@ -181,6 +75,20 @@ abline(3.3837,-0.01181,col=cols[3],lwd=3)
 legend('topleft',col=brewer.pal(3,'Dark2'),
        legend = c("Master MTurk","non-Master MTurk","Course Credit"),lwd= c(3,3,3),
        cex=0.5,pt.cex = 0.5)
+
+
+#Generate accuracy over time plot
+#does accuracy change as time goes by
+plot(per_user_image$question.ordinal,per_user_image$fmean,ylim=c(0,1),col="#00000033",pch=20,xlab = 'Question Ordinal Index',
+     ylab = 'Mean F-value')
+cols = brewer.pal(3,"Dark2")
+#Master pvalue=0.0457
+abline(0.7909,-0.00008,col=cols[1],lwd=3)
+#Turker pvalue<0.0001
+abline(0.8100,-0.00027,col=cols[2],lwd=3)
+#SONA pvalue<0.0001
+abline(0.7256,-0.00095,col=cols[3],lwd=3)
+legend('bottomleft',col=brewer.pal(3,'Dark2'),legend = c("Master MTurk","non-Master MTurk","Course Credit"),lwd= c(3,3,3))
 
 
 ###Fmean vs logtime
